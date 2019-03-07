b5ff76e426319bccd4db53881ee6023b82bbfbfe
tomoya-yokota <tomoya-yokota@cybozu.co.jp>
2019-03-07 13:46:36 +0900

    add garoon.base.user.getLoginUser

--- src/garoon.d.ts
+++ src/garoon.d.ts
@@ -5,4 +5,21 @@ declare namespace garoon {
             handler: (event: any) => any
         ): void;
     }
+    namespace base {
+        namespace user {
+            function getLoginUser(): LoginUser;
+            // TODO nullable check
+            interface LoginUser {
+                id: string;
+                garoonId: string;
+                code: string;
+                name: string;
+                email: string;
+                url: string;
+                phone: string;
+                timezone: string;
+                language: string;
+            }
+        }
+    }
 }
