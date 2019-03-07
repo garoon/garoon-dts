9dface98e6734f5e521edfa07d246432381b4052
youtr-s <suzuki_yutaro@comture.com>
2019-03-07 16:11:04 +0900

    fix typo

--- src/test/garoon.ts
+++ src/test/garoon.ts
@@ -1,3 +1,6 @@
 // garoon.base.user.getLoginUser
 const user = garoon.base.user.getLoginUser();
 const s: string = user.id;
+
+const scheduleEvent = garoon.schedule.event.get();
+const id: string = scheduleEvent.id
